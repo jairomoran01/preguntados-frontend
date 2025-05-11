ce51092ee5f1f5a68cd8c7d67794b4b5507f979a
MozartCuidadorDigital <mozartcuidadordigital@gmail.com>
2025-05-11 15:35:47 -0500

    vercel

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    sourcemap: true
+  },
+  server: {
+    port: 3000,
+    strictPort: true,
+    host: true
+  }
 })
